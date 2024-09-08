ef963f48e5d31efd3fa9540f28d4ca8c9297d463
omp10 <omparteki2002@gmail.com>
2024-09-08 14:33:43 +0530

    logo fixed

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import express from "express";
+import path from "path"
+;import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
@@ -11,6 +12,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname=path.resolve();
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
@@ -20,10 +23,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
+
 // Root route
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 // Start server
 server.listen(PORT, () => {
